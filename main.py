9e9e582de4ca5e1de27859250ddd1f6ea2a9c511
hannah627e <hannah06@uw.edu>
2022-02-26 23:42:14 -0800

    more work with the api

--- main.py
+++ main.py
@@ -27,25 +27,25 @@ def van_gogh_topics(df):
     """
     Comment
     """
-    print('started van_gogh_topics')
+    print('started van_gogh_topics - this may take a while')
     terms = {}
 
     # should we define this outside this function? won't be referenced anywhere but is technically a constant
     MET_MUSEUM_API = 'https://collectionapi.metmuseum.org/public/collection/v1'
     paintings_ids = requests.get(MET_MUSEUM_API + '/search?q=Van_Gogh')
     for id in paintings_ids.json()['objectIDs']:
-        # print(id)
         painting_info = requests.get(MET_MUSEUM_API + '/objects/' + str(id))
-        for tag in painting_info.json()['tags']:
-            print(tag['term'])
-            """
-            term = tag['term']
-            if term in terms:
-                terms[term] += 1
-            else:
-                terms[term] = 1
-            """
+        if painting_info.json()['tags']:
+            for tag in painting_info.json()['tags']:
+                term = tag['term']
+                if term in terms:
+                    terms[term] += 1
+                else:
+                    terms[term] = 1
     print(terms)
+    # next: sort dictionary
+    # splice for top 10
+    # graph with plotly
 
 
 def main():
